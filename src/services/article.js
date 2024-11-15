825c79387a31541f3100d06b3eed27f6287d379c
IK-251293 <ikalinic2512@gmail.com>
2024-11-15 14:51:38 +0100

    Add files via upload

---
+++ src/services/article.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const articleApi = createApi({
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "x-rapidapi-key",
+        "0e7aab8d1bmsh3eb7d1aa0951976p159152jsnff8fc2d091e9"
+      );
+      headers.set(
+        "x-rapodapi-host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) =>
+        `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+    }),
+  }),
+});
+
+export const { useLazyGetSummaryQuery } = articleApi;
